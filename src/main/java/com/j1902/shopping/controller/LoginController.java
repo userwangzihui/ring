acc2e5a1defba43fb8363c9609551eaeca081cc1
wangYang <1440443950@qq.com>
2019-06-26 11:09:58 +0800

    frontLogin

---
+++ src/main/java/com/j1902/shopping/controller/LoginController.java
@@ -0,0 +1,91 @@
+package com.j1902.shopping.controller;
+
+import com.j1902.shopping.pojo.User;
+import com.j1902.shopping.service.UserService;
+import com.j1902.shopping.utils.JsonUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/frontLogin")
+    public String login(HttpServletRequest request, Map<String, Object> map, HttpServletResponse response) throws UnsupportedEncodingException {
+        Cookie[] cookies = request.getCookies();
+        User cookUser = null;
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie c : cookies) {
+                if ("REMEMBER_PASSWORD".equals(c.getName())) {
+                    String userJson = c.getValue();
+                    userJson = URLDecoder.decode(userJson, "UTF-8");
+                    cookUser = JsonUtils.jsonToPojo(userJson, User.class);
+                    map.put("phone", cookUser.getUserPhone());
+                    map.put("password", cookUser.getUserPwd());
+                }
+                if ("addCookie".equals(c.getName())) {
+                    userService.deleteCookie(request, response);
+                    return "front/login";
+                }
+            }
+        }
+        return "front/login";
+    }
+
+    @RequestMapping("/log")
+    @ResponseBody
+    public Map<String, Object> log(HttpServletRequest request) {
+        Map<String, Object> map = new HashMap<>();
+        Cookie[] cookies = request.getCookies();
+        User cookUser = null;
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                if ("REMEMBER_PASSWORD".equals(cookie.getName())) {
+                    map.put("is", 1);
+                    return map;
+                } else if ("addCookie".equals(cookie.getName())) {
+                    map.put("is", 1);
+                    return map;
+                }
+            }
+        }
+        return null;
+    }
+
+    @RequestMapping("/logins")
+    public String login(User user, HttpServletRequest request, HttpServletResponse response, Map<String, Object> map) throws UnsupportedEncodingException {
+        String userphone = request.getParameter("userphone");
+        String rememberPassword = request.getParameter("rememberPassword");
+        System.out.println("rememberPassword = " + rememberPassword);
+        System.out.println("user = " + user.toString());
+        if (userService.login(user)) {
+            if ("true".equals(rememberPassword)) {
+                userService.rememberPwd(user, response);
+            } else {
+                userService.deleteCookie(request, response);
+            }
+            return "front/index";
+        } else if ("true".equals(rememberPassword)) {
+            userService.addCookie(user.getUserPhone(), response);
+            map.put("phone", user.getUserPhone());
+            map.put("loginFail", "用户名与密码不匹配！");
+            return "front/login";
+        }
+        map.put("phone", user.getUserPhone());
+        map.put("loginFail", "用户名与密码不匹配！");
+        return "front/login";
+    }
+
+}
---
+++ src/main/java/com/j1902/shopping/service/UserService.java
@@ -0,0 +1,18 @@
+package com.j1902.shopping.service;
+
+import com.j1902.shopping.pojo.User;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+
+public interface UserService {
+
+    public boolean login(User user);
+
+    public void  rememberPwd(User user, HttpServletResponse response) throws UnsupportedEncodingException;
+
+    public void deleteCookie(HttpServletRequest request, HttpServletResponse response);
+
+    public void addCookie(String user, HttpServletResponse response) throws UnsupportedEncodingException;
+}
